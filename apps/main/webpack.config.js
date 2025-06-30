90af595758dbb32aa3757cc325df71779c32cfb2
Aliaksandr Famchankou <aliaksandr.famchankou@gmail.com>
2025-06-30 07:27:20 +0200

    feat: update build configs

--- apps/main/webpack.config.js
+++ apps/main/webpack.config.js
@@ -1,20 +1,47 @@
 const { NxAppWebpackPlugin } = require('@nx/webpack/app-plugin');
+const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
+// const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
 const { join } = require('path');
 
 module.exports = {
   output: {
     path: join(__dirname, '../../dist/apps/main'),
+    filename: 'main.js',
   },
+  resolve: {
+    extensions: ['.ts', '.js', '.json'],
+    plugins: [
+      new TsconfigPathsPlugin({
+        configFile: join(__dirname, '../../tsconfig.build.json'),
+      }),
+    ],
+    alias: {
+      '@shared/core': join(__dirname, '../../libs/core/index.ts'),
+    },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        include: [
+          join(__dirname, '../../libs/core'),
+          join(__dirname, 'src'),
+        ],
+        use: 'ts-loader',
+      },
+    ],
+  },
+  externals: {},
   plugins: [
     new NxAppWebpackPlugin({
       target: 'node',
-      compiler: 'tsc',
       main: './src/main.ts',
+      // compiler: 'tsc',
       tsConfig: './tsconfig.app.json',
       assets: ['./src/assets', './src/README.md'],
       optimization: false,
       outputHashing: 'none',
-      generatePackageJson: true,
     }),
+    // new BundleAnalyzerPlugin(),
   ],
 };
---
+++ libs/core/src/index.ts
@@ -0,0 +1,7 @@
+export * from './entities';
+export * from './db/db.module';
+export * from './config';
+export * from './utils';
+export * from './dtos';
+export * from './domain';
+export * from './mappers';
